2aea04cc0458e43410ebaad90c07551018705eff
Students <Students@DESKTOP-C0DHI8H>
2025-01-27 11:33:37 +0200

    Images

--- Shop11A/Controllers/HomeController.cs
+++ Shop11A/Controllers/HomeController.cs
@@ -15,9 +15,9 @@ namespace Shop11A.Controllers
 
         public List<Product> products = new List<Product>
         {
-            new Product{ Id = 1, Name = "Tablet PGMT 11a", Description = "Super mega giga tablet PGMT 11a", Price = 2000.11M, ImageUrl = "/images/t1.jpg" },
-            new Product{ Id = 2, Name = "Tablet PGMT 11b", Description = "Super mega giga tablet PGMT 11b", Price = 2234.11M, ImageUrl = "/images/t2.jpg" },
-            new Product{ Id = 3, Name = "Tablet PGMT 12b", Description = "Super mega giga tablet PGMT 12b", Price = 2500.11M, ImageUrl = "/images/t3.jpg" },
+            new Product{ Id = 1, Name = "Keyboard PGMT 11a", Description = "Keyboard PGMT 11a", Price = 500.11M, ImageUrl = "/images/t1.jpg" },
+            new Product{ Id = 2, Name = "PC PGMT 11b", Description = "Super mega giga PC PGMT 11b", Price = 2000.11M, ImageUrl = "/images/t2.jpg" },
+            new Product{ Id = 3, Name = "Mouse PGMT 12b", Description = "Mouse PGMT 12b", Price = 200.11M, ImageUrl = "/images/t3.jpg" },
 
             new Product{ Id = 4, Name = "MehanoPhone 11a", Description = "Super mega giga MehanoPhone 11a", Price = 1500.11M, ImageUrl = "/images/p1.jpg" },
             new Product{ Id = 5, Name = "MehanoPhone 11b", Description = "Super mega giga MehanoPhone 11b", Price = 1600.11M, ImageUrl = "/images/p2.jpg" },
